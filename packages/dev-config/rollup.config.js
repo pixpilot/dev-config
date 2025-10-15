9df5367be6aeddf28eea2ce8ddd7447b4343ce4a
ccpu <m.doaie@hotmail.com>
2025-10-15 21:05:18 +0100

    feat(rollup-config): add additional copy targets for configuration
    * Included `commitlint.js` and `prettier.js` in the copy targets.
    * Enhances the build process by ensuring these files are available in the distribution directory.

--- packages/dev-config/rollup.config.js
+++ packages/dev-config/rollup.config.js
@@ -7,6 +7,8 @@ export default rollupConfig({
     targets: [
       { src: 'src/tsconfig-base.json', dest: 'dist' },
       { src: 'src/markdownlint.json', dest: 'dist' },
+      { src: 'src/commitlint.js', dest: 'dist' },
+      { src: 'src/prettier.js', dest: 'dist' },
     ],
   },
 });
